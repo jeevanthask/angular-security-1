59a82c09545fe98d4f4a983df8fc324761df6828
jeevanthask <jeevanthanandika@gmail.com>
2020-08-16 23:02:04 +0530

    upgrade

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 
-router.get('/',(req,res) => {
+mongoose.connect('mongodb://127.0.0.1:27017/AngularAuth', {useNewUrlParser: true})
+const connection = mongoose.connection
+
+connection.once('open', function () {
+    console.log('mongodb connection established successfully')
+})
+
+
+router.get('/', (req, res) => {
     res.send('hi all')
 })
 
