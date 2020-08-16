b2e43a3327612ac00b0895a94de4237fdf267165
jeevanthask <jeevanthanandika@gmail.com>
2020-08-16 22:29:59 +0530

    'upgrade'

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const PORT = 3000
+const api = require('./routes/api')
 
 const app = express()
 
 app.use(bodyParser.json())
+
+app.use('/api',api)
 
 app.get('/',(req,res) => {
     res.send('welcome all guys')
