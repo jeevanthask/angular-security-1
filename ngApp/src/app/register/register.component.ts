1cd8b4283d160d18596c1a46dc49632dce24b85b
jeevanthask <jeevanthanandika@gmail.com>
2020-08-17 16:00:56 +0530

    upgrade

--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-register',
@@ -13,13 +14,20 @@ export class RegisterComponent implements OnInit {
     password:''
   }
 
-  constructor() {
+  constructor(private _auth: AuthService,private _router: Router) {
   }
 
   ngOnInit() {
   }
 
   registerUser() {
-    console.log(this.registerUserData)
+    this._auth.registerUser(this.registerUserData)
+      .subscribe(
+        res => {
+          localStorage.setItem('token', res.token)
+          this._router.navigate(['/special'])
+        },
+        err => console.log(err)
+      )
   }
 }
