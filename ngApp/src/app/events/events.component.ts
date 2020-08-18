ea2189a020192a5ff84546eda5991a4373452349
jeevanthask <jeevanthanandika@gmail.com>
2020-08-18 09:59:11 +0530

    upgrade

--- ngApp/src/app/events/events.component.ts
+++ ngApp/src/app/events/events.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EventService} from "../event.service";
 
 @Component({
   selector: 'app-events',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsComponent implements OnInit {
 
-  constructor() { }
+  events: Object = []
 
-  ngOnInit(): void {
+  constructor(private _eventService: EventService) {
+  }
+
+  ngOnInit() {
+    this._eventService.getEvents()
+      .subscribe(
+        res => this.events = res,
+        err => console.log(err)
+      )
   }
 
 }
