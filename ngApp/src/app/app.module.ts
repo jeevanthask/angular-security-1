4171b10e7b6467134db98fcde999fcacb653e520
jeevanthask <jeevanthanandika@gmail.com>
2020-08-18 21:42:05 +0530

    upgrade

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { EventsComponent } from './events/events.component';
-import { SpecialEventsComponent } from './special-events/special-events.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {EventsComponent} from './events/events.component';
+import {SpecialEventsComponent} from './special-events/special-events.component';
 import {RouterModule, Routes} from "@angular/router";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AuthService} from "./auth.service";
@@ -16,7 +16,7 @@ import {AuthGuard} from "./auth.guard";
 
 const routes: Routes = [
   {path: 'events', component: EventsComponent},
-  {path: 'special', component: SpecialEventsComponent},
+  {path: 'special', component: SpecialEventsComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
 
@@ -40,7 +40,8 @@ const routes: Routes = [
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [AuthService,EventService,AuthGuard],
+  providers: [AuthService, EventService, AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
